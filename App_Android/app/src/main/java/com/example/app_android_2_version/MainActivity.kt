373a29728f6928605bc776bf4d947803179e3261
MiF318 <mif318@mail.ru>
2024-04-13 14:04:17 +0500

    task

--- App_Android/app/src/main/java/com/example/app_android_2_version/MainActivity.kt
+++ App_Android/app/src/main/java/com/example/app_android_2_version/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.app_android_2_version
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,11 +15,16 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.app_android_2_version.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var Task: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +41,47 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_home, R.id.navigation_waterdrop, R.id.navigation_training, R.id.navigation_tasks))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val taskList = findViewById<ListView>(R.id.tasks_tasklist)
+        val newTask: EditText = findViewById(R.id.tasks_newtask)
+        val createTask: Button = findViewById(R.id.tasks_createTask)
+
+        //Настройка списка
+        val actualTasks: MutableList<String> = mutableListOf()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, actualTasks)
+        taskList.adapter = adapter
+
+        val calendar = Calendar.getInstance()
+
+        //Удаление по клику на элемент
+        taskList.setOnItemClickListener { parent, view, position, id ->
+            val text = taskList.getItemAtPosition(position).toString()
+            adapter.remove(text)
+        }
+
+        //Добавление задачи по клику
+        createTask.setOnClickListener {
+            Task = newTask.text.toString().trim()
+
+            val date = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateLabel(calendar)
+            }
+
+            if(Task != ""){
+                DatePickerDialog(this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
+                adapter.insert(Task, 0)
+            }
+        }
     }
 
+    private fun updateLabel(calendar: Calendar) {
+        val format = "dd-mm-yyyy"
+        val std = SimpleDateFormat(format, Locale.TRADITIONAL_CHINESE)
+        Task.plus(std.format(calendar.time).toString())
+    }
 
 
     fun onButtonclick(view: View)
